55e3391fdb7fa3f99cd570c28c57641034bda01d
Vaibhavi Baware <vaibhavibaware09@gmail.com>
2025-06-09 01:05:56 +0530

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-
 export default function Contact() {
     return (
         <div className="relative flex items-top justify-center min-h-[700px] bg-white sm:items-center sm:pt-0">
